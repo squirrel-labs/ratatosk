92e8bb7523c775014ccf68355e3f0178ebf4a61c
TrueDoctor <d-kobert@web.de>
2018-09-26 19:18:41 +0200

    implemented fudamental Web Api

---
+++ DSACore/Models/Command.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DSACore.Models
+{
+    public class Command
+    {
+        public string Name { get; set; }
+        public string CmdText { get; set; }
+    }
+}
