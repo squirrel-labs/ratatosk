c298da2f85fe4ff36c9defb27b48368377a5b687
TrueKuehli <rctcoaster2000@hotmail.de>
2018-09-27 21:37:23 +0200

    Changed things

--- WebInterface/NodeJSServer/src/index.js
+++ WebInterface/NodeJSServer/src/index.js
@@ -4,4 +4,8 @@ import * as signalR from "@aspnet/signalr";
 let backdrop = new Backdrop('menu', 'front-layer', 'show-menu');
 backdrop.register();
 
-console.log('HI')
+const connection = new signalR.HubConnectionBuilder()
+    .withUrl("https://89.183.31.151:5001/chatHub")
+    .configureLogging(signalR.LogLevel.Information)
+    .build();
+connection.start().catch(err => console.error(err.toString()));
