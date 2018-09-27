f8018831b42fa9f534494babcd496f77e4709166
TrueDoctor <d-kobert@web.de>
2018-09-27 17:43:56 +0200

    added SignalR

--- DSACore/Startup.cs
+++ DSACore/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DSACore.Hubs;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -26,6 +27,8 @@ namespace DSACore
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddSignalR();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -41,6 +44,7 @@ namespace DSACore
             }
 
             app.UseHttpsRedirection();
+            app.UseSignalR(routes => { routes.MapHub<ChatHub>("/chatHub"); });
             app.UseMvc();
         }
     }
