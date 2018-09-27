9bb9e2ca95652e9f8fd3f21bf0206808de803b43
TrueKuehli <rctcoaster2000@hotmail.de>
2018-09-27 22:26:20 +0200

    Changed things & stuff

--- WebInterface/NodeJSServer/src/index.js
+++ WebInterface/NodeJSServer/src/index.js
@@ -5,7 +5,32 @@ let backdrop = new Backdrop('menu', 'front-layer', 'show-menu');
 backdrop.register();
 
 const connection = new signalR.HubConnectionBuilder()
-    .withUrl("https://89.183.31.151:5001/chatHub")
+    .withUrl("http://89.183.31.151:5000/chatHub")
     .configureLogging(signalR.LogLevel.Information)
     .build();
+
+connection.on('ReceiveMessage', (user, message) => {
+  let msg = message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
+  let encodedMsg = user + " says " + msg;
+  let li = document.createElement("li");
+  li.textContent = encodedMsg;
+  document.getElementById('message-list').appendChild(li);
+});
+
+document.getElementById('send-button').addEventListener('click', () => {
+  let user = document.getElementById("user-input").value;
+  let message = document.getElementById("message-input").value;
+  connection.invoke("SendMessage", user, message).catch(function (err) {
+    return console.error(err.toString());
+  });
+  event.preventDefault();
+});
+
+
+
+
+
+
+
+
 connection.start().catch(err => console.error(err.toString()));
