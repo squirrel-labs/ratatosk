abd51697be6ca4580c15929ccb2569a1e984ef88
TrueKuehli <rctcoaster2000@hotmail.de>
2018-09-29 21:51:02 +0200

    Did some code splitting and added Server Listing
    Split code into different modules
    Added method to fetch server list from remote server, and to populate this into the server list of the UI

--- WebInterface/NodeJSServer/src/index.js
+++ WebInterface/NodeJSServer/src/index.js
@@ -1,6 +1,6 @@
 import Backdrop from './modules/ui/backdrop.js';
-import * as signalR from '@aspnet/signalr';
 import BannerController from './modules/ui/notification-banner.js';
+import ServerClient from './modules/server-client.js'
 
 let backdrop = new Backdrop('menu', 'front-layer', 'show-menu');
 backdrop.register();
@@ -9,30 +9,7 @@ let notifications = new BannerController('notifications',
     'banner-info', 'dismiss-banner');
 notifications.register();
 
-const connection = new signalR.HubConnectionBuilder()
-    .withUrl("http://89.183.117.197:5000/chatHub")
-    .configureLogging(signalR.LogLevel.Information)
-    .build();
+let client = new ServerClient('http://89.183.117.197:5000/chatHub', 'server-list', true);
+document.getElementById('refresh-button')
+    .addEventListener('click', client.loadServers.bind(client));
 
-connection.on('ReceiveMessage', (user, message) => {
-  let msg = message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
-  let encodedMsg = user + " says " + msg;
-  let li = document.createElement("div");
-  li.classList.add('server');
-  li.textContent = encodedMsg;
-  document.getElementById('server-list').appendChild(li);
-});
-
-document.getElementById('new-game-button').addEventListener('click', () => {
-  let method = window.prompt('Please enter method:', 'SendMessage');
-  let user = window.prompt('Please enter user:', 'Default');
-  let message = window.prompt('Please enter message:', 'Super duper Nachricht');
-  connection.invoke(method, user, message).catch(function (err) {
-    return console.error(err.toString());
-  });
-  event.preventDefault();
-}),
-
-connection.start()
-    .then(() => console.log('Connected'))
-    .catch(err => console.error(err.toString()));
---
+++ WebInterface/NodeJSServer/src/modules/server-client.js
@@ -0,0 +1,67 @@
+import * as signalR from '@aspnet/signalr';
+import ServerListing from './ui/server-listing.js';
+
+export default class ServerClient {
+  constructor(url, serverListingId, debug = false) {
+    const connectionBuilder = new signalR.HubConnectionBuilder()
+        .withUrl(url)
+
+    if (debug) {
+      connectionBuilder.configureLogging(signalR.LogLevel.Debug);
+    } else {
+      connectionBuilder.configureLogging(signalR.LogLevel.Error);
+    }
+
+    this.connection = connectionBuilder.build();
+    this.connection.start()
+        .then(() => this.loadServers())
+        .catch(() => err => console.error(err.toString()));
+
+    this.refreshing = false;
+
+    this.serverListing = new ServerListing(serverListingId);
+  }
+
+  loadServers() {
+    if (this.refreshing) return;
+    this.connection.on('ListGroups', (groups) => {
+      console.log(groups)
+      this.serverListing.flushElements();
+      this.serverListing.addElements(groups);
+      this.connection.off('ListGroups');
+
+      this.refreshing = false;
+    });
+
+    this.connection.invoke('GetGroups')
+        .catch(err => {
+          this.refreshing = false;
+          console.error(err.toString())
+        });
+    this.refreshing = true;
+  }
+
+  createServer(){
+
+  }
+
+  messageHandling(){
+    this.connection.on('ReceiveMessage', (user, message) => {
+      let msg = message.replace(/&/g, "&amp;")
+          .replace(/</g, "&lt;")
+          .replace(/>/g, "&gt;");
+      let encodedMsg = user + " sagt:  " + msg;
+    });
+  }
+}
+
+
+
+// connection.on('ReceiveMessage', (user, message) => {
+//   let msg = message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
+//   let encodedMsg = user + " says " + msg;
+//   let li = document.createElement("div");
+//   li.classList.add('server');
+//   li.textContent = encodedMsg;
+//   document.getElementById('server-list').appendChild(li);
+// });
