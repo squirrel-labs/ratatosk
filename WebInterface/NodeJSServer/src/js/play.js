9f033e0f24f55af5a48039f06cdf0b8513fa2bda
TrueDoctor <d-kobert@web.de>
2019-05-05 18:57:18 +0200

    Update play.js

--- WebInterface/NodeJSServer/src/js/play.js
+++ WebInterface/NodeJSServer/src/js/play.js
@@ -2,7 +2,7 @@ import Interface from './modules/interface';
 import UIManager from './modules/ui/uiManager';
 import Networker from './modules/networking/networker';
 
-const SERVERURL = 'http://127.0.0.1:5000/chatHub';
+const SERVERURL = 'http://dsa_core_1:5000/chatHub';
 
 let iface = new Interface();
 let uiMan = new UIManager(iface);
