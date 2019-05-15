eedd564c1490a1cc785a1a8bbfdc3e5987543e52
TrueDoctor <d-kobert@web.de>
2019-05-15 23:05:47 +0200

    change chathub url

--- WebInterface/NodeJSServer/src/js/play.js
+++ WebInterface/NodeJSServer/src/js/play.js
@@ -2,7 +2,7 @@ import Interface from './modules/interface';
 import UIManager from './modules/ui/uiManager';
 import Networker from './modules/networking/networker';
 
-const SERVERURL = 'https://kobert.dev/dsa/api/chatHub';
+const SERVERURL = 'https://kobert.dev/api/Login';
 
 let iface = new Interface();
 let uiMan = new UIManager(iface);
