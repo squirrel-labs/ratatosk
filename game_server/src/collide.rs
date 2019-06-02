7d0f3cf2bec7a248aed699805542ad235e354753
Dennis Kobert <d-kobert@web.de>
2019-06-02 13:57:54 +0200

    Fix rotated hitbox point interaction
    implement according tests

--- game_server/src/collide.rs
+++ game_server/src/collide.rs
@@ -25,24 +25,35 @@ impl Collide<AABox> for AABox {
 
 impl Collide<Vec2> for RBox {
     fn collides(&self, other: &Vec2) -> bool {
-        let v1_diff = *other + self.v1 * (-self.v1.scalar(&other) / self.v1.distance2());
-        let v2_diff = *other + self.v2 * (-self.v2.scalar(&other) / self.v2.distance2());
+        let v1_diff = *other + self.v1 * (-self.v1.scalar(&(*other - self.pos)) / self.v1.distance2());
+        let v2_diff = *other + self.v2 * (-self.v2.scalar(&(*other - self.pos)) / self.v2.distance2());
 
-        self.pos < v1_diff && v1_diff < self.pos + self.v2
-        && self.pos < v2_diff && v2_diff < self.pos + self.v1
+        let v1_dist = ((v1_diff - self.pos) / self.v2).x;
+        let v2_dist = ((v2_diff - self.pos) / self.v1).x;
+        0.0 <= v1_dist && v2_dist <= 1.0
+        && 0.0 <= v2_dist && v2_dist <= 1.0
+        //v1_diff < self.pos + self.v2 && self.pos < v1_diff
+        //&& v2_diff < self.pos + self.v1 && self.pos < v2_diff
     }
 }
 
 impl Collide<AABox> for RBox {
     fn collides(&self, other: &AABox) -> bool {
+        let other_size = other.pos + other.size;
         let v1_diff = other.pos + self.v1 * (-self.v1.scalar(&other.pos) / self.v1.distance2());
         let v2_diff = other.pos + self.v2 * (-self.v2.scalar(&other.pos) / self.v2.distance2());
-        let other_size = other.pos + other.size;
         let v1_diff_size = other_size + self.v1 * (-self.v1.scalar(&other_size) / self.v1.distance2());
         let v2_diff_size = other_size + self.v2 * (-self.v2.scalar(&other_size) / self.v2.distance2());
 
-        self.pos < v1_diff + other.size && v1_diff < self.pos + self.v2
-        && self.pos < v2_diff + other.size && v2_diff < self.pos + self.v1
+        let v1_dist = ((v1_diff - self.pos) / self.v2).x;
+        let v2_dist = ((v2_diff - self.pos) / self.v1).x;
+        let v1_dist_size = ((v1_diff_size - self.pos) / self.v2).x;
+        let v2_dist_size = ((v2_diff_size - self.pos) / self.v1).x;
+
+        0.0 <= v1_dist_size && v1_dist <= 1.0
+        && 0.0 <= v2_dist_size && v2_dist <= 1.0
+        //v1_diff < self.pos + self.v2 && self.pos < v1_diff_size
+        //&& v2_diff < self.pos + self.v1 && self.pos < v2_diff_size
     }
 }
 
@@ -80,11 +91,7 @@ impl<S, T: Collide<S>> Collide<S> for Vec<T> {
         }
     
         #[test]
-        fn test_not_collide_aabox_aabox() {
-             let a = Vec2{x: 1.0, y: 7.5};
-             let b = Vec2{x: 3.0, y: 2.5};
-             let c = Vec2{x: 0.5, y: 5.0};
-             let aa_box = AABox{pos: a, size: b};
+        fn test_not_collide_aabox_dot() {
              let a = Vec2{x: 1.0, y: 7.5};
              let b = Vec2{x: 3.0, y: 2.5};
              let c = Vec2{x: 0.5, y: 5.0};
@@ -92,23 +99,63 @@ impl<S, T: Collide<S>> Collide<S> for Vec<T> {
 
              assert!(!(aa_box.collides(&c)));
         }
+        
+        #[test]
+        fn test_collide_aabox_aabox_intersecting() {
+             let a = Vec2{x: 1.0, y: 2.5};
+             let b = Vec2{x: 3.0, y: 2.5};
+             let aa_box = AABox{pos: a, size: b};
+             let a = Vec2{x: 2.0, y: 3.5};
+             let b = Vec2{x: 3.0, y: 7.5};
+             let bb_box = AABox{pos: a, size: b};
+
+             assert!(aa_box.collides(&bb_box));
+        }
+
+        #[test]
+        fn test_collide_aabox_aabox_crossed() {
+             let a = Vec2{x: 2.0, y: 0.5};
+             let b = Vec2{x: 1.0, y: 7.5};
+             let aa_box = AABox{pos: a, size: b};
+             let a = Vec2{x: 1.0, y: 3.5};
+             let b = Vec2{x: 5.0, y: 4.5};
+             let bb_box = AABox{pos: a, size: b};
+
+             assert!(aa_box.collides(&bb_box));
+        }
+    
+        #[test]
+        fn test_not_collide_aabox_aabox() {
+             let a = Vec2{x: 1.0, y: 1.0};
+             let b = Vec2{x: 1.0, y: 1.0};
+             let aa_box = AABox{pos: a, size: b};
+             let a = Vec2{x: 3.0, y: 3.5};
+             let b = Vec2{x: 3.0, y: 7.5};
+             let bb_box = AABox{pos: a, size: b};
+
+             assert!(!(aa_box.collides(&bb_box)));
+        }
 
         #[test]
         fn test_collide_Rbox_dot() {
-             let a = Vec2{x: 1.0, y: 2.5};
-             let b = Vec2{x: 3.0, y: 7.5};
-             let c = Vec2{x: 1.5, y: 5.0};
-             let aa_box = AABox{pos: a, size: b};
+             let a = Vec2{x: 1.0, y: 1.0};
+             let b = Vec2{x: 1.0, y: 1.0};
+             let c = Vec2{x: 1.0, y: -1.0};
+             let aa_box = RBox{pos: a, v1: b, v2: c};
+
+             let c = Vec2{x: 1.6, y: 0.6};
 
              assert!(aa_box.collides(&c));
         }
     
         #[test]
         fn test_not_collide_Rbox_dot() {
-             let a = Vec2{x: 1.0, y: 7.5};
-             let b = Vec2{x: 3.0, y: 2.5};
-             let c = Vec2{x: 0.5, y: 5.0};
-             let aa_box = AABox{pos: a, size: b};
+             let a = Vec2{x: 1.0, y: 1.0};
+             let b = Vec2{x: 1.0, y: 1.0};
+             let c = Vec2{x: 1.0, y: -1.0};
+             let aa_box = RBox{pos: a, v1: b, v2: c};
+
+             let c = Vec2{x: 1.4, y: 0.4};
 
              assert!(!(aa_box.collides(&c)));
         }
