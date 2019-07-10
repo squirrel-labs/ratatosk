d6435699e462799621ec2c07956f9fd5e0493a26
konsumlamm <konsumlamm@gmail.com>
2019-07-10 12:46:07 +0200

    add proposed impl comments

--- webhogg/game_engine/src/math/mat2.rs
+++ webhogg/game_engine/src/math/mat2.rs
@@ -1,7 +1,45 @@
 use std::ops;
 
+use crate::math::Vec2;
+
 /// A 2x2 matrix.
 #[derive(Clone, Copy, Debug)]
 pub struct Mat2 {
     data: [f32; 4],
 }
+
+/*
+impl ops::Add for Mat2 {}
+
+impl ops::AddAssign for Mat2 {}
+
+impl ops::Sub for Mat2 {}
+
+impl ops::SubAssign for Mat2 {}
+
+impl ops::Neg for Mat2 {}
+
+impl ops::Mul<f32> for Mat2 {}
+
+impl ops::MulAssign<f32> for Mat2 {}
+
+impl ops::Mul<Vec2> for Mat2 {}
+
+impl ops::MulAssign<Vec2> for Mat2 {}
+
+impl ops::Mul for Mat2 {}
+
+impl ops::MulAssign for Mat2 {}
+
+impl ops::Div<f32> for Mat2 {}
+
+impl ops::DivAssign<f32> for Mat2 {}
+
+impl Mat2 {
+    pub fn zero() -> Self {}
+
+    pub fn identity() -> Self {}
+
+    pub fn transpose(self) -> Self {}
+}
+*/
--- webhogg/game_engine/src/math/mat3.rs
+++ webhogg/game_engine/src/math/mat3.rs
@@ -1,7 +1,46 @@
 use std::ops;
 
+use crate::math::Vec3;
+
 /// A 3x3 matrix.
 #[derive(Clone, Copy, Debug)]
 pub struct Mat3 {
     data: [f32; 9],
 }
+
+/*
+impl ops::Add for Mat3 {}
+
+impl ops::AddAssign for Mat3 {}
+
+impl ops::Sub for Mat3 {}
+
+impl ops::SubAssign for Mat3 {}
+
+impl ops::Neg for Mat3 {}
+
+impl ops::Mul<f32> for Mat3 {}
+
+impl ops::MulAssign<f32> for Mat3 {}
+
+impl ops::Mul<Vec3> for Mat3 {}
+
+impl ops::MulAssign<Vec3> for Mat3 {}
+
+impl ops::Mul for Mat3 {}
+
+impl ops::MulAssign for Mat3 {}
+
+impl ops::Div<f32> for Mat3 {}
+
+impl ops::DivAssign<f32> for Mat3 {}
+
+impl Mat3 {
+    pub fn zero() -> Self {}
+
+    pub fn identity() -> Self {}
+
+    pub fn transpose(self) -> Self {}
+}
+*/
+
