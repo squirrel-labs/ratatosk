376c18439b71e7aca2a31c2b79e9a223b081f33b
konsumlamm <konsumlamm@gmail.com>
2019-07-10 12:45:06 +0200

    add pub uses

--- webhogg/game_engine/src/math/mod.rs
+++ webhogg/game_engine/src/math/mod.rs
@@ -3,7 +3,11 @@ pub mod mat3;
 pub mod vec2;
 pub mod vec3;
 
-use crate::math::vec2::Vec2;
+pub use mat2::Mat2;
+pub use mat3::Mat3;
+pub use vec2::Vec2;
+pub use vec3::Vec3;
+
 use std::ops;
 
 pub const EPSILON: f32 = 1e-8;
