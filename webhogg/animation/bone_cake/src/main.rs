eb9612ff561978f57b893285d7941ac2829d2a76
Dennis Kobert <dennis@kobert.dev>
2019-07-12 19:52:57 +0200

    Fix formatting

--- webhogg/animation/bone_cake/src/main.rs
+++ webhogg/animation/bone_cake/src/main.rs
@@ -5,8 +5,10 @@ use std::env;
 fn main() {
     let mut args = env::args();
     if args.len() != 2 {
-        println!("Wrong number of arguments specified!\n{}",
-                 "Usage: bone_cake [Name_tex.json]");
+        println!(
+            "Wrong number of arguments specified!\n{}",
+            "Usage: bone_cake [Name_tex.json]"
+        );
         std::process::exit(1);
     }
     let t = textures::load_char(args.next_back().unwrap());
