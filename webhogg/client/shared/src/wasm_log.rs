ccf9e6953c76778dce400d57eb40f1e80779bc40
Dennis Kobert <dennis@kobert.dev>
2019-07-21 16:42:13 +0200

    Implement console_log! macro

--- webhogg/client/shared/src/wasm_log.rs
+++ webhogg/client/shared/src/wasm_log.rs
@@ -1,5 +1,10 @@
 use wasm_bindgen::prelude::*;
 
+#[macro_export]
+macro_rules! console_log {
+    ($($t:tt)*) => (webhogg_wasm_shared::wasm_log::log(&format_args!($($t)*).to_string()))
+}
+
 #[wasm_bindgen]
 extern "C" {
     #[wasm_bindgen(js_namespace=console)]
