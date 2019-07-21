a978eee8a5a87e87fb7133d23a693cd4efe13790
natrixaeria <janng@gmx.de>
2019-07-21 22:29:58 +0200

    Add test for initial empty memory

--- webhogg/client/double_buffer/tests/integration.rs
+++ webhogg/client/double_buffer/tests/integration.rs
@@ -1,6 +1,8 @@
 use double_buffer::DoubleBuffer;
 
 #[test]
-fn test_create_new() {
-    let _db: DoubleBuffer<u32> = DoubleBuffer::new();
+fn test_initial_empty() {
+    let mut db: DoubleBuffer<u32> = DoubleBuffer::new();
+
+    assert!(db.borrow_reader().is_none());
 }
