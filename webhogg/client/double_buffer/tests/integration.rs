b1c7af30a22487ca8dc1bb019d584b73227d63c7
natrixaeria <janng@gmx.de>
2019-07-21 22:43:47 +0200

    Add simple read write test

--- webhogg/client/double_buffer/tests/integration.rs
+++ webhogg/client/double_buffer/tests/integration.rs
@@ -6,3 +6,16 @@ fn test_initial_empty() {
 
     assert!(db.borrow_reader().is_none());
 }
+
+#[test]
+fn test_simple_read_write() {
+    let mut db = DoubleBuffer::new();
+    {
+        let mut writer = db.borrow_writer();
+        writer.set(42);
+    }
+    {
+        let reader = db.borrow_reader().unwrap();
+        assert_eq!(*reader.get(), 42);
+    }
+}
