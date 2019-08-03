943814aac2640138c2747c6e2e198a4b8c17a0c8
Dennis Kobert <dennis@kobert.dev>
2019-08-03 20:30:24 +0200

    Use clap to parse commandline arguments

--- game_server/src/main.rs
+++ game_server/src/main.rs
@@ -9,12 +9,23 @@ mod game_logger;
 
 #[macro_use]
 extern crate log;
+#[macro_use]
+extern crate clap;
+use clap::App;
+use std::error::Error;
 
-fn main() {
+fn main() -> Result<(), Box<dyn Error>> {
     game_logger::init_logger();
 
-    let addr = ("0.0.0.0", 5001);
+    let yaml = load_yaml!("cli.yml");
+    let matches = App::from_yaml(yaml).get_matches();
+
+    let addr = matches.value_of("address").unwrap_or("127.0.0.1");
+    let port = matches.value_of("port").unwrap_or("5001").parse()?;
+
+    let addr = (addr, port);
     info!("create game server on {:?}", addr);
     let mut gameserver = server::GameServer::new(addr);
     gameserver.run().unwrap();
+    Ok(())
 }
