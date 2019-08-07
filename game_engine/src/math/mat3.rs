5b69cbfb6ffe7b9e17f690ce61610a83f64dcbfe
natrixaeria <janng@gmx.de>
2019-08-08 01:34:47 +0200

    Change matmul algorithm to Ladermann
    and implement AsRef for mat3

--- game_engine/src/math/mat3.rs
+++ game_engine/src/math/mat3.rs
@@ -141,36 +141,51 @@ impl ops::Mul<Vec3> for Mat3 {
 impl ops::Mul for Mat3 {
     type Output = Self;
 
-    fn mul(self, other: Self) -> Self::Output {
-        Self::new(
-            self.data[0] * other.data[0]
-                + self.data[3] * other.data[1]
-                + self.data[6] * other.data[2],
-            self.data[1] * other.data[0]
-                + self.data[4] * other.data[1]
-                + self.data[7] * other.data[2],
-            self.data[2] * other.data[0]
-                + self.data[5] * other.data[1]
-                + self.data[8] * other.data[2],
-            self.data[0] * other.data[3]
-                + self.data[3] * other.data[4]
-                + self.data[6] * other.data[5],
-            self.data[1] * other.data[3]
-                + self.data[4] * other.data[4]
-                + self.data[7] * other.data[5],
-            self.data[2] * other.data[3]
-                + self.data[5] * other.data[4]
-                + self.data[8] * other.data[5],
-            self.data[0] * other.data[6]
-                + self.data[3] * other.data[7]
-                + self.data[6] * other.data[8],
-            self.data[1] * other.data[6]
-                + self.data[4] * other.data[7]
-                + self.data[7] * other.data[8],
-            self.data[2] * other.data[6]
-                + self.data[5] * other.data[7]
-                + self.data[8] * other.data[8],
-        )
+    /// Laderman Algorithm
+    /// see:
+    /// http://www.ams.org/journals/bull/1976-82-01/S0002-9904-1976-13988-2/S0002-9904-1976-13988-2.pdf
+    fn mul(self, b: Self) -> Self::Output {
+        let a = move |y: usize, x: usize| self.data[x + 3*y - 4];
+        let b = move |y: usize, x: usize| b.data[x + 3*y - 4];
+
+        let m1 = (a(1,1) + a(1,2) + a(1,3) - a(2,1) - a(2,2) - a(3,2) - a(3,3)) * b(2,2);
+        let m2 = (a(1,1) - a(2,1)) * (b(2,2) - b(1,2));
+        let m3 = a(2,2) * (-b(1,1) + b(1,2) + b(2,1) - b(2,2) - b(2,3) - b(3,1) + b(3,3));
+        let m4 = (-a(1,1) + a(2,1) + a(2,2)) * (b(1,1) - b(1,2) + b(2,2));
+        let m5 = (a(2,1) + a(2,2)) * (b(1,2) - b(1,1));
+        let m6 = a(1,1) * b(1,1);
+        let m7 = (-a(1,1) + a(3,1) + a(3,2)) * (b(1,1) - b(1,3) + b(2,3));
+        let m8 = (-a(1,1) + a(3,1)) * (b(1,3) - b(2,3));
+        let m9 = (a(3,1) + a(3,2)) * (-b(1,1) + b(1,3));
+        let m10 = (a(1,1) + a(1,2) + a(1,3) - a(2,2) - a(2,3) - a(3,1) - a(3,2)) * b(2,3);
+        let m11 = a(3,2) * (-b(1,1) + b(1,3) + b(2,1)
+                            - b(2,2) - b(2,3) - b(3,1) + b(3,2));
+        let m12 = (-a(1,3) + a(3,2) + a(3,3)) * (b(2,2) + b(3,1) - b(3,2));
+        let m13 = (a(1,3) - a(3,3)) * (b(2,2) - b(3,2));
+        let m14 = a(1,3) * b(3,1);
+        let m15 = (a(3,2) + a(3,3)) + (-b(3,1) + b(3,2));
+        let m16 = (-a(1,3) + a(2,2) + a(2,3)) * (b(2,3) + b(3,1) - b(3,3));
+        let m17 = (a(1,3) - a(2,3)) * (b(2,3) - b(3,3));
+        let m18 = (a(2,2) + a(2,3)) * (-b(3,1) + b(3,3));
+        let m19 = a(1,2) * b(2,1);
+        let m20 = a(2,3) * b(3,2);
+        let m21 = a(2,1) * b(1,3);
+        let m22 = a(3,1) * b(1,2);
+        let m23 = a(3,3) * b(3,3);
+
+        Self {
+            data: [
+                m6 + m14 + m19,
+                m1 + m4 + m5 + m6 + m12 + m14 + m15,
+                m6 + m7 + m9 + m10 + m14 + m16 + m18,
+                m2 + m3 + m4 + m6 + m14 + m16 + m17,
+                m2 + m4 + m5 + m6 + m20,
+                m14 + m16 + m17 + m18 + m21,
+                m6 + m7 + m8 + m11 + m12 + m13 + m14,
+                m12 + m13 + m14 + m15 + m22,
+                m6 + m7 + m8 + m9 + m23
+            ],
+        }
     }
 }
 
@@ -216,7 +231,7 @@ impl Mat3 {
     /// Creates a new Mat3.
     pub fn new(a: f32, b: f32, c: f32, d: f32, e: f32, f: f32, g: f32, h: f32, i: f32) -> Self {
         Self {
-            data: [a, b, c, d, e, f, g, h, i],
+            data: [a, d, g, b, e, h, c, f, i],
         }
     }
 
@@ -249,12 +264,12 @@ impl Mat3 {
     pub fn rotation(angle: f32) -> Self {
         let cos = angle.cos();
         let sin = angle.sin();
-        Self::new(cos, sin, 0.0, -sin, cos, 0.0, 0.0, 0.0, 1.0)
+        Self::new(cos, -sin, 0.0, sin, cos, 0.0, 0.0, 0.0, 1.0)
     }
 
     /// Returns a matrix that scales by `scale`.
-    pub fn scaling(scale: f32) -> Self {
-        Self::new(scale, 0.0, 0.0, 0.0, scale, 0.0, 0.0, 0.0, scale)
+    pub fn scaling(sx: f32, sy: f32) -> Self {
+        Self::new(sx, 0.0, 0.0, 0.0, sy, 0.0, 0.0, 0.0, 1.0)
     }
 
     /// Returns a matrix that translates by (`x`, `y`).
@@ -266,14 +281,20 @@ impl Mat3 {
     pub fn transpose(self) -> Self {
         Self::new(
             self.data[0],
-            self.data[3],
-            self.data[6],
             self.data[1],
-            self.data[4],
-            self.data[7],
             self.data[2],
+            self.data[3],
+            self.data[4],
             self.data[5],
+            self.data[6],
+            self.data[7],
             self.data[8],
         )
+    }
+}
+
+impl std::convert::AsRef<[f32; 9]> for Mat3 {
+    fn as_ref(&self) -> &[f32; 9] {
+        &self.data
     }
 }
