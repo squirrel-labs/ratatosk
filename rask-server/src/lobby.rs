308d0355e2b55124523be4289e6ce97c036ea0c7
konsumlamm <konsumlamm@gmail.com>
2019-09-04 23:11:03 +0200

    impl Sender and Receiver functions for Connection

--- rask-server/src/lobby.rs
+++ rask-server/src/lobby.rs
@@ -1,14 +1,15 @@
-use std::sync::mpsc::{channel, Receiver, Sender};
+use std::sync::mpsc;
+use std::time::Duration;
 
-struct Connection<T> {
-    sender: Sender<T>,
-    receiver: Receiver<T>,
+pub struct Connection<T> {
+    sender: mpsc::Sender<T>,
+    receiver: mpsc::Receiver<T>,
 }
 
 impl<T> Connection<T> {
-    fn new() -> (Self, Self) {
-        let (tx1, rx1) = channel();
-        let (tx2, rx2) = channel();
+    pub fn new() -> (Self, Self) {
+        let (tx1, rx1) = mpsc::channel();
+        let (tx2, rx2) = mpsc::channel();
         (Self {
             sender: tx1,
             receiver: rx2,
@@ -18,13 +19,37 @@ impl<T> Connection<T> {
             receiver: rx1,
         })
     }
+
+    pub fn send(&self, t: T) -> Result<(), mpsc::SendError<T>> {
+        self.sender.send(t)
+    }
+
+    pub fn try_recv(&self) -> Result<T, mpsc::TryRecvError> {
+        self.receiver.try_recv()
+    }
+
+    pub fn recv(&self) -> Result<T, mpsc::RecvError> {
+        self.receiver.recv()
+    }
+
+    pub fn recv_timeout(&self, timeout: Duration) -> Result<T, mpsc::RecvTimeoutError> {
+        self.receiver.recv_timeout(timeout)
+    }
+
+    pub fn iter(&self) -> mpsc::Iter<T> {
+        self.receiver.iter()
+    }
+
+    pub fn try_iter(&self) -> mpsc::TryIter<T> {
+        self.receiver.try_iter()
+    }
 }
 
-struct Lobby {
+pub struct Lobby {
     connection: Connection<String>,
 }
 
-struct Listener {
+pub struct Listener {
     connections: Vec<Connection<String>>,
 }
 
