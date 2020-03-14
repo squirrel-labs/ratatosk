6ab1c9d886c650765a755de1e22a67f7689d0ebc
konsumlamm <konsumlamm@gmail.com>
2020-03-15 00:09:49 +0100

    add EngineError

--- rask-engine/src/lib.rs
+++ rask-engine/src/lib.rs
@@ -6,3 +6,18 @@ pub mod boxes;
 pub mod collide;
 pub mod math;
 pub mod world;
+
+use std::error::Error;
+use std::fmt::{self, Display};
+
+/// The error type used by the game engine.
+#[derive(Debug)]
+pub enum EngineError {}
+
+impl Display for EngineError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        todo!()
+    }
+}
+
+impl Error for EngineError {}
