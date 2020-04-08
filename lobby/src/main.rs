b612920596c6a581ad6280ac9c7032da34d23e0d
Maximilian Bosch <maximilian@mbosch.me>
2020-04-08 23:06:59 +0200

    Allow CORS for any origin in dev builds

--- lobby/src/main.rs
+++ lobby/src/main.rs
@@ -1,6 +1,8 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
 #[macro_use] extern crate rocket;
+use rocket::fairing::AdHoc;
+use rocket::http::Header;
 
 #[get("/")]
 fn index() -> &'static str {
@@ -8,5 +10,10 @@ fn index() -> &'static str {
 }
 
 fn main() {
-    rocket::ignite().mount("/", routes![index]).launch();
+    rocket::ignite().mount("/", routes![index])
+        .attach(AdHoc::on_response("CORS header for dev env", |req, res| {
+            #[cfg(debug_assertions)]
+            res.set_header(Header::new("Access-Control-Allow-Origin", "*"));
+        }))
+        .launch();
 }
