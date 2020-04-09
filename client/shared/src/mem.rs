457dc0c983e9ab7b7075151f64d0542d98d90c7b
NatrixAeria <upezu@student.kit.edu>
2020-04-09 20:40:06 +0200

    Add message queue implementation

--- client/shared/src/mem.rs
+++ client/shared/src/mem.rs
@@ -11,6 +11,7 @@ const fn MiB(n: usize) -> usize {
 }
 
 const STACK_ALIGNMENT: usize = 1024 * 63;
+const MESSAGE_QUEUE_LENGTH: usize = 32;
 
 /// The size of the stack. Its start is at address 0
 pub const GRAPHIC_STACK_SIZE: usize = MiB(4) + STACK_ALIGNMENT;
@@ -63,6 +64,65 @@ impl SynchronizationMemory {
         while self.last_elapsed_ms == self.elapsed_ms {
             wait_until_wake_up_at((&mut self.elapsed_ms) as *mut i32)
         }
+    }
+}
+
+#[repr(align(4))]
+struct MessageQueueElement<T: Sized + Clone> {
+    reading: u8,
+    writing: u8,
+    payload: T,
+}
+
+impl<T: Sized + Clone> MessageQueueElement<T> {
+    fn set_reading(&mut self, val: u8) {
+        unsafe { atomic_write_u8(&mut self.reading, val) }
+    }
+
+    fn get_writing(&self) -> u8 {
+        unsafe { atomic_read_u8(&self.writing) }
+    }
+    
+    fn read(&mut self) -> Option<T> {
+        self.set_reading(1);
+        if self.get_writing() == 0 {
+            let e = self.payload.clone();
+            self.set_reading(0);
+            Some(e)
+        } else { None }
+    }
+}
+
+#[repr(align(4))]
+pub struct MessageQueue<T: Sized + Clone> {
+    /// the index of the next element to be written
+    writer_index: u32,
+    /// the index of the next element to be read
+    reader_index: u32,
+    _phantom: core::marker::PhantomData<T>
+}
+
+impl<T: Sized + Clone> MessageQueue<T> {
+    pub fn length() -> usize {
+        MESSAGE_QUEUE_LENGTH
+    }
+
+    fn mem_size() -> usize {
+        core::mem::size_of::<T>() * Self::length() + core::mem::size_of::<Self>()
+    }
+
+    unsafe fn get_mut(&mut self, n: usize) -> Option<&mut MessageQueueElement<T>> {
+        core::slice::from_raw_parts_mut((self as *mut Self as *mut u8).offset(core::mem::size_of::<Self>() as isize) as *mut MessageQueueElement<T>, Self::length()).get_mut(n)
+    }
+
+    pub fn pop(&mut self) -> Option<T> {
+        let e = unsafe { self.get_mut(self.reader_index as usize)? };
+        let e = e.read()?;
+        self.reader_index += 1;
+        if self.reader_index as usize >= Self::length() {
+            self.reader_index = 0;
+        }
+        Some(e)
     }
 }
 
@@ -127,6 +187,14 @@ pub unsafe fn atomic_read_u8(ptr: *const u8) -> u8 {
 
 pub unsafe fn atomic_read_i32(ptr: *const i32) -> i32 {
     (*(ptr as *const core::sync::atomic::AtomicI32)).load(core::sync::atomic::Ordering::SeqCst)
+}
+
+pub unsafe fn atomic_write_u32(ptr: *mut u32, v: u32) {
+    (*(ptr as *mut core::sync::atomic::AtomicU32)).store(v, core::sync::atomic::Ordering::SeqCst)
+}
+
+pub unsafe fn atomic_read_u32(ptr: *const u32) -> u32 {
+    (*(ptr as *const core::sync::atomic::AtomicU32)).load(core::sync::atomic::Ordering::SeqCst)
 }
 
 pub fn wait_until_wake_up_at(ptr: *mut i32) {
