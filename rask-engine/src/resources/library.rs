7e16f99da6cb00220599a8f4d0718c2b384e6dde
Dennis Kobert <dennis@kobert.dev>
2020-04-12 00:14:01 +0200

    Rename Library to ResourceTable

--- rask-engine/src/resources/library.rs
+++ rask-engine/src/resources/library.rs
@@ -1,26 +1,20 @@
 use super::Resource;
 use crate::EngineError;
 
-/// Size of the internal catalog.
-/// This determines the highest available id.
-const CATALOG_SIZE: usize = 512;
-
 /// The library is used to store and retrieve resources.
-pub struct Library {
-    catalog: &'static mut [Resource],
-}
+pub struct ResourceTable(&'static mut [Resource]);
 
 macro_rules! get_store {
     ($type: ty, $enum_type: ident) => {
-        impl GetStore<$type> for Library {
+        impl GetStore<$type> for ResourceTable {
             unsafe fn get(&'static self, id: usize) -> Result<&'static $type, EngineError> {
-                match &self.catalog[id] {
+                match &self.0[id] {
                     Resource::$enum_type(value) => Ok(&value),
                     _ => Err("Wrong resource type".into()),
                 }
             }
             unsafe fn store(&'static mut self, data: $type, id: usize) {
-                self.catalog[id] = Resource::$enum_type(data);
+                self.0[id] = Resource::$enum_type(data);
             }
         }
     };
@@ -41,21 +35,22 @@ pub trait GetStore<T> {
     unsafe fn store(&'static mut self, data: T, id: usize);
 }
 
-impl Library {
+impl ResourceTable {
     /// Create a new library at a specific position in memory.
     ///
     /// # Safety
     ///
     /// The function is safe as long as the memory from memory_offset to memory_offset + CATALOG_SIZE * sizeof(Resource)
-    pub unsafe fn new(memory_offset: usize) -> Self {
-        Library {
-            catalog: core::slice::from_raw_parts_mut(memory_offset as *mut Resource, CATALOG_SIZE),
-        }
+    pub unsafe fn new(memory_offset: usize, catalog_size: usize) -> Self {
+        ResourceTable(core::slice::from_raw_parts_mut(
+            memory_offset as *mut Resource,
+            catalog_size,
+        ))
     }
 
     pub unsafe fn init(&mut self) {
-        for i in 0..CATALOG_SIZE {
-            self.catalog[i] = Resource::None;
+        for i in 0..self.0.len() {
+            self.0[i] = Resource::None;
         }
     }
 }
--- rask-engine/src/resources/mod.rs
+++ rask-engine/src/resources/mod.rs
@@ -21,12 +21,12 @@ fn test() {
 ```
 */
 
-pub mod library;
+mod library;
 pub mod sound;
 pub mod texture;
 
 #[doc(inline)]
-pub use library::Library;
+pub use library::*;
 #[doc(inline)]
 pub use sound::Sound;
 #[doc(inline)]
