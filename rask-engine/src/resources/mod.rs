a0a473aa292b9c59c910073462524b89f80102af
konsumlamm <konsumlamm@gmail.com>
2020-04-12 02:01:18 +0200

    Fix collision detection
    rename variables in tests
    add ratatosk.iml to .gitignore

--- rask-engine/src/resources/mod.rs
+++ rask-engine/src/resources/mod.rs
@@ -8,12 +8,10 @@ use lazy_static::lazy_static;
 # use rask_engine::resources::*;
 
 lazy_static! {
-    static ref LIB: Library = unsafe { Library::new(0) };
+    static ref LIB: ResourceTable = unsafe { ResourceTable::new(0, 0) };
 }
 
 fn test() {
-    use library::GetStore;
-
     unsafe {
         let _texture: &Texture = LIB.get(0).unwrap();
     }
