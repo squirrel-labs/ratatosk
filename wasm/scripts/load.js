1f4740cdb6afe259c5de2f3654c79c8be562dacb
Dennis Kobert <dennis@kobert.dev>
2020-05-11 20:37:07 +0200

    Remove unnecessary files

--- wasm/scripts/load.js
+++
@@ -1,8 +0,0 @@
-    async function load(module, imports, maybe_memory) {
-        memory = imports.wbg.memory = maybe_memory;
-        const instance = await WebAssembly.instantiate(module, imports);
-        //instance.exports.init_alloc();
-
-        return { instance, module };
-    }
-
