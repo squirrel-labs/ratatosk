c9ecae5c440fa4c0b1af68ac67458846ef376a90
NatrixAeria <upezu@student.kit.edu>
2020-05-17 17:58:12 +0200

    Apply review feedback

--- wasm/src/graphics/webgl/error.rs
+++ wasm/src/graphics/webgl/error.rs
@@ -1,44 +1,36 @@
 use super::bindings::Gl2;
 
-#[derive(Debug)]
-pub enum WebGl2Error {
-    ContextLost,
-    InvalidEnum,
-    InvalidValue,
-    InvalidOperation,
-    InvalidFramebufferOperation,
-    OutOfMemory,
-    UnknownError,
-}
-
-impl From<u32> for WebGl2Error {
-    fn from(v: u32) -> Self {
-        match v {
-            Gl2::INVALID_ENUM => WebGl2Error::InvalidEnum,
-            Gl2::INVALID_VALUE => WebGl2Error::InvalidValue,
-            Gl2::INVALID_OPERATION => WebGl2Error::InvalidOperation,
-            Gl2::INVALID_FRAMEBUFFER_OPERATION => WebGl2Error::InvalidFramebufferOperation,
-            Gl2::OUT_OF_MEMORY => WebGl2Error::OutOfMemory,
-            Gl2::CONTEXT_LOST_WEBGL => WebGl2Error::ContextLost,
-            _ => WebGl2Error::UnknownError,
+macro_rules! error_variants {
+    ($iv:vis $st:ident { $($v:ident($c:expr) -> $s:literal),+ }) => {
+        #[repr(u32)]
+        #[derive(Debug)]
+        $iv enum $st {
+            $($v = $c),+
         }
-    }
+        impl From<u32> for $st {
+            fn from(v: u32) -> Self {
+                match v {
+                    $(x if x == $st::$v as u32 => $st::$v),+,
+                    _ => $st::UnknownError,
+                }
+            }
+        }
+        impl std::fmt::Display for $st {
+            fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+                write!(f, "{}", match self {
+                    $($st::$v => $s),+
+                })
+            }
+        }
+    };
 }
 
-impl std::fmt::Display for WebGl2Error {
-    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        write!(
-            f,
-            "{}",
-            match self {
-                WebGl2Error::ContextLost => "lost webgl context",
-                WebGl2Error::InvalidEnum => "invalid enum",
-                WebGl2Error::InvalidValue => "invalid value",
-                WebGl2Error::InvalidOperation => "invalid operation",
-                WebGl2Error::InvalidFramebufferOperation => "invalid framebuffer operation",
-                WebGl2Error::OutOfMemory => "out of memory",
-                WebGl2Error::UnknownError => "unknown webgl2 error",
-            }
-        )
-    }
-}
+error_variants!(pub WebGl2Error {
+    ContextLost(Gl2::CONTEXT_LOST_WEBGL) -> "lost webgl context",
+    InvalidEnum(Gl2::INVALID_ENUM) -> "invalid enum",
+    InvalidValue(Gl2::INVALID_VALUE) -> "invalid value",
+    InvalidOperation(Gl2::INVALID_OPERATION) -> "invalid operation",
+    InvalidFramebufferOperation(Gl2::INVALID_FRAMEBUFFER_OPERATION) -> "invalid framebuffer operation",
+    OutOfMemory(Gl2::OUT_OF_MEMORY) -> "out of memory",
+    UnknownError(0xff) -> "unknown webgl2 error"
+});
