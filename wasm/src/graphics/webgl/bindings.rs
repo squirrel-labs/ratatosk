0e5dd11067a9db40506802a6de819ad96c4c5f05
NatrixAeria <upezu@student.kit.edu>
2020-05-18 19:57:16 +0200

    Add shader comilation and attachment

--- wasm/src/graphics/webgl/bindings.rs
+++ wasm/src/graphics/webgl/bindings.rs
@@ -1,4 +1,6 @@
+use super::shader::ShaderType;
 use crate::ClientError;
+use std::convert::TryInto;
 
 pub struct Gl2;
 
@@ -10,6 +12,8 @@ impl Gl2 {
     pub const INVALID_FRAMEBUFFER_OPERATION: u32 = 1286;
     pub const NO_ERROR: u32 = 0;
     pub const OUT_OF_MEMORY: u32 = 1285;
+    pub const FRAGMENT_SHADER: u32 = 35632;
+    pub const VERTEX_SHADER: u32 = 35633;
 
     pub fn get_error(&self) -> u32 {
         unsafe { gl_get_error() }
@@ -27,17 +31,58 @@ impl Gl2 {
             _ => unreachable!("unexpected return value from js function"),
         }
     }
+
+    pub fn create_program(&self) -> Result<u32, ClientError> {
+        unsafe { gl_create_program() }.try_into().map_err(|_| {
+            ClientError::WebGlError("glCreateProgram returned an unexpected object".to_string())
+        })
+    }
+
+    pub fn attach_new_shader(&self, prog: u32, shader_type: ShaderType) -> Result<(), ClientError> {
+        match unsafe { gl_attach_new_shader(prog as i32, shader_type as u32) } {
+            0 => Ok(()),
+            1 => Err(ClientError::WebGlError(
+                "shader creation failed, invalid program handle".to_string(),
+            )),
+            2 => Err(ClientError::WebGlError(
+                "shader creation failed, invalid/unknown shader type".to_string(),
+            )),
+            3 => Err(ClientError::WebGlError(
+                "shader creation failed, glCreateShader returned an unexpected object".to_string(),
+            )),
+            4 => Err(ClientError::WebGlError(
+                "shader compilation failed".to_string(),
+            )),
+            _ => unreachable!("unexpected return value from js function"),
+        }
+    }
 }
 
 extern "C" {
     /// same as `WebGLRenderingContext.getError()`
     fn gl_get_error() -> u32;
+
     /// This function creates a vertex array with a vertex buffer.
-    /// It returns an error code:
+    /// Return values are:
     ///     * 0 - success
     ///     * 1 - failiure, VAO creation failed
     ///     * 2 - failiure, VBO creation failed
     /// # Safety
     /// The pointer must be 32bit aligned
     fn gl_create_vertex_array_and_buffer_with_data(data: *const f32, len32: usize) -> u32;
+
+    /// This function creates a shader program.
+    /// Return values are:
+    ///     * positive: success, a program handle was returned
+    ///     * negative: failiure, an error occured
+    fn gl_create_program() -> i32;
+
+    /// This function compiles a shader and attaches it to a shader program.
+    /// Return values are:
+    ///     * 0 - success
+    ///     * 1 - failiure, invalid/unknown program handle
+    ///     * 2 - failiure, invalid/unknown shader type
+    ///     * 3 - failiure, shader creation failed
+    ///     * 4 - failiure, shader compilation failed
+    fn gl_attach_new_shader(prog: i32, shader_type: u32) -> u32;
 }
--- wasm/src/graphics/webgl/shader.rs
+++ wasm/src/graphics/webgl/shader.rs
@@ -1,7 +1,29 @@
-const VERTEX_SHADER: &str = include_str!("shader/vertex.glsl");
-const FRAGMENT_SHADER: &str = include_str!("shader/fragment.glsl");
+use super::Gl2;
+use crate::ClientError;
 
-enum ShaderType {
-    Vertex,
-    Fragment,
+#[repr(u32)]
+pub enum ShaderType {
+    Vertex = Gl2::VERTEX_SHADER,
+    Fragment = Gl2::FRAGMENT_SHADER,
+}
+
+/// A WebGL Shader program is a program running on an GPU device.
+/// It is linked from multiple shaders.
+pub struct ShaderProgram {
+    handle: u32,
+    transform_loc_index: u32,
+    texture_loc_index: u32,
+}
+
+impl ShaderProgram {
+    pub fn new(gl: &Gl2) -> Result<Self, ClientError> {
+        let prog = gl.create_program()?;
+        gl.attach_new_shader(prog, ShaderType::Vertex)?;
+        gl.attach_new_shader(prog, ShaderType::Fragment)?;
+        Ok(Self {
+            handle: prog,
+            transform_loc_index: 0, // TODO
+            texture_loc_index: 0,   // TODO
+        })
+    }
 }
