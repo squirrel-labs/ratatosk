a5470f2fd914257b69f44b60a61b6607b7b5abdf
Dennis Kobert <dennis@kobert.dev>
2020-06-22 09:38:34 +0200

    Add javascript fetch_resource api

--- rask-engine/src/network/protocol/op_codes.rs
+++ rask-engine/src/network/protocol/op_codes.rs
@@ -13,7 +13,7 @@ pub const MEMORY_OFFSETS: u32 = 10;
 pub const SET_TEXT_MODE: u32 = 11;
 pub const PUSH_RESOURCE: u32 = 12;
 pub const FETCH_RESOURCE: u32 = 13;
-pub const PUSH_GAME_STATE: u32 = 13;
-pub const PUSH_ENGINE_EVENT: u32 = 15;
-pub const PUSH_SERVER_EVENT: u32 = 16;
+pub const PUSH_GAME_STATE: u32 = 14;
+pub const PUSH_ENGINE_EVENT: u32 = 16;
+pub const PUSH_SERVER_EVENT: u32 = 17;
 // The following lines are inserted from `wasm/scripts/main.js`
